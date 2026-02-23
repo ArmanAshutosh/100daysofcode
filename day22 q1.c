574746b653367c0178433b15b031c15201ba33c2
ArmanAshutosh <armanashutosh9@gmail.com>
2026-02-23 18:58:24 +0530

    day22 q1.c
    Implement linked list node creation and counting.

---
+++ day22 q1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count nodes
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    
+    return count;
+}
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("%d", countNodes(head));
+
+    return 0;
+}

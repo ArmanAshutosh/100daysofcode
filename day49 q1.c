5cbbdb19cf2fd0583e0c76620fdb68659a181f41
ArmanAshutosh <armanashutosh9@gmail.com>
2026-03-22 23:56:37 +0530

    day49 q1.c

---
+++ day49 q1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for BST node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+// Inorder traversal (sorted output)
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    int n, x;
+    struct Node* root = NULL;
+
+    // Input number of elements
+    scanf("%d", &n);
+
+    // Insert elements into BST
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    // Print inorder traversal
+    inorder(root);
+
+    return 0;
+}
